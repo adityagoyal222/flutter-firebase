0c079a1754301f1442fbe7e6361f247d8bf155ee
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-06-17 23:48:59 +0545

    added remaining funcs/ completed

--- flutterfirebase/lib/screens/home/home.dart
+++ flutterfirebase/lib/screens/home/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfirebase/screens/home/brew_list.dart';
+import 'package:flutterfirebase/screens/home/settings_form.dart';
 import 'package:flutterfirebase/services/auth.dart';
+import 'package:flutterfirebase/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:flutterfirebase/models/brew.dart';
 
 class Home extends StatelessWidget {
 
@@ -7,23 +12,51 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text(
-            'Brew Crew',
-          ),
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-              onPressed: () async {
-                await _auth.signOut();
-              } ,
+
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: SettingsForm(),
+        );
+      });
+    }
+
+    return StreamProvider<List<Brew>>.value(
+        value: DatabaseService().brews,
+        child: Scaffold(
+          backgroundColor: Colors.brown[50],
+          appBar: AppBar(
+            title: Text(
+              'Brew Crew',
             ),
-          ],
-        ));
+            backgroundColor: Colors.brown[400],
+            elevation: 0.0,
+            actions: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                label: Text('logout'),
+                onPressed: () async {
+                  await _auth.signOut();
+                } ,
+              ),
+              FlatButton.icon(
+                icon: Icon(Icons.settings),
+                label: Text('Settings',),
+                onPressed: () => _showSettingsPanel(),
+              ),
+            ],
+          ),
+          body: Container(
+            child: BrewList(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/coffee_bg.png'),
+                fit: BoxFit.cover,
+              )
+            ),
+          ),
+        ),
+    );
   }
 }
